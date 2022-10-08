5a801b7293cf30dbd1065a4c79b13c29ea94fde2
Galactc <davidblankenburg@hotmail.com>
2022-10-08 14:38:55 -0500

    ore

--- Final GameJam Product/Assets/mineral.cs
+++ Final GameJam Product/Assets/mineral.cs
@@ -6,38 +6,85 @@ public class mineral : MonoBehaviour
 {
     float oreCurrency = 0;
     public float OCT = 0;
+    public float Ops = 0;
+
+    float leafCurrency = 0;
+    public float LCT = 0;
+    public float Lps = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("currencychangeore", 0, 1f);
+        InvokeRepeating("currencychangeleaf", 0, 1f);
+        InvokeRepeating("callcurrency", 0, 1f);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(oreCurrency);
-        currencychange();
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            OCT += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            LCT += 1;
+        }
     }
-    void currencychange()
+    void currencychangeore()
     {
 
-        oreCurrency += 1;
+        oreCurrency += Ops;
 
 
         if (OCT == 1)
         {
-            oreCurrency += 1;
+            Ops = 1;
         }
 
         if (OCT == 2)
         {
-            oreCurrency += 3;
+            Ops = 3;
         }
 
-        if (OCT == 3)
+        if (OCT == 5) 
         {
-            oreCurrency += 5;
+            Ops = 5;
         }
 
     }
+    void callcurrency()
+    {
+        Debug.Log(oreCurrency+" ore");
+       
+        Debug.Log(oreCurrency+" leaf");
+       
+    }
+    void currencychangeleaf()
+    {
+
+        oreCurrency += Lps;
+
+
+        if (LCT == 1)
+        {
+            Lps = 1;
+        }
+
+        if (LCT == 2)
+        {
+            Lps = 3;
+        }
+
+        if (LCT == 3)
+        {
+            Lps = 5;
+        }
+       
+    }
 }
+// Oct= ore current tier
+// Ops = ore per sec
+// Lct = leaf current tier
+// Lps = leaf per second
