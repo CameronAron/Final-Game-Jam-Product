93d50b146233af39e2e7c63ab0c122966e79190f
Galactc <davidblankenburg@hotmail.com>
2022-10-09 11:28:25 -0500

    10/9/22

--- Final GameJam Product/Assets/Menutogame.cs
+++ Final GameJam Product/Assets/Menutogame.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class Menutogame : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -14,10 +14,14 @@ public class Menutogame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SceneManager.LoadScene("End");
+      
     }
     private void OnMouseDown()
     {
-      
+        lod();
+    }
+   public void lod()
+    {
+        SceneManager.LoadScene("Main");
     }
 }
